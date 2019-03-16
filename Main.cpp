80933d917e9e1b8e3fe6bc101049788bcb6857c7
Sarah Pell <spell1@uvm.edu>
2019-03-16 01:59:20 -0400

    not sure what's going wrong, moved some things around and pushing to see if it helps ( no functionality was added/changed, just moved lines around to push to git) ??? try pulling again. all files that are needed should be included though hmmm

--- Main.cpp
+++ Main.cpp
@@ -19,10 +19,10 @@ int main(){
     string fileName;
     string sequences[2] = {};
 
+    cout<<"Pairwise Sequence Alignment!!!"<<endl;
     //keep running program until the user wants to exit (enters 1)
     while(choice != 1){
         //ask user for file name
-        cout<<"Pairwise Sequence Alignment"<<endl;
         cout<<"What is the name of your file?"<<endl;
         cin>>fileName;
 
@@ -38,19 +38,19 @@ int main(){
                 count++;
             }
             file.close();
+            cout<<"--------------------------------------------"<<endl;
+            //global alignment
+            globalAlign(sequences[0], sequences[1]);
+            cout<<"--------------------------------------------"<<endl;
+            //local alignment
+            localAlign(sequences[0], sequences[1]);
+            cout<<"--------------------------------------------"<<endl;
+            cout<<"would you like to try another file?"<<endl<<"hit any number to continue"<< endl<<"1 to exit"<<endl;
+            cin>>choice;
         }
         else{
             cout<<"unable to find file"<<endl;
         }
-        cout<<"--------------------------------------------"<<endl;
-        //global alignment
-        globalAlign(sequences[0], sequences[1]);
-        cout<<"--------------------------------------------"<<endl;
-        //local alignment
-        localAlign(sequences[0], sequences[1]);
-        cout<<"--------------------------------------------"<<endl;
-        cout<<"would you like to try another file?"<<endl<<"hit any number to continue"<< endl<<"1 to exit"<<endl;
-        cin>>choice;
     }
 
 
